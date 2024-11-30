76ccb2447a564d5e102cf17544e938f135427b4e
Vanz-silva <vanz_silva@icloud.com>
2024-11-29 21:34:26 -0300

    Carrousel logos

--- src/app/sections/Skills.jsx
+++ src/app/sections/Skills.jsx
@@ -1,26 +1,82 @@
-import SkillCard from "../components/SkillCard";
-import "bootstrap/dist/css/bootstrap.min.css";
-const Skills = () => (
-  <section id="habilidades">
-    <div className="container text-center logos-tec">
-      <div className="row box-texto-logos text-center">
-        <h2>Skills</h2>
-      </div>
+"use client"; // Diretiva para habilitar o uso de client-side
 
-      <div
-        className="row align-items-center  d-flex justify-content-center"
-        id="linha-logos"
-      >
-        <SkillCard src="/img/logoHtml.png" alt="CSS" />
-        <SkillCard src="/img/logoCss.png" alt="CSS" />
-        <SkillCard src="/img/logoJs.png" alt="Javascript" />
-        <SkillCard src="/img/logoReact.png" alt="React" />
-        <SkillCard src="/img/logoBoot.png" alt="Bootstrap" />
-        <SkillCard src="/img/logoJs.png" alt="Javascript" />
-        <SkillCard src="/img/logoGit.png" alt="HTML" />
+import SkillCard from "../components/SkillCard"; // Mantido o import original
+import "bootstrap/dist/css/bootstrap.min.css"; // Estilos do Bootstrap
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // JS do Bootstrap para o carrossel
+
+const Skills = () => {
+  return (
+    <section id="habilidades">
+      <div className="container text-center logos-tec">
+        <div className="row box-texto-logos text-center">
+          <h2>Skills</h2>
+        </div>
+
+        {/* Carrossel do Bootstrap */}
+        <div
+          id="carouselSkills"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            {/* Primeira Slide */}
+            <div className="carousel-item active">
+              <div className="row justify-content-center">
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+              </div>
+            </div>
+
+            {/* Segunda Slide */}
+            <div className="carousel-item">
+              <div className="row justify-content-center">
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+              </div>
+            </div>
+
+            {/* Terceira Slide */}
+            <div className="carousel-item">
+              <div className="row justify-content-center">
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+                <div className="col-3 logo-container">
+                  <SkillCard src="/img/logoHtml.png" alt="HTML" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Skills;
