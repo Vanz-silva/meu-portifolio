23eabf15e11a81f10c6b4476a0ed669ee5824bb5
Vanz-silva <vanz_silva@icloud.com>
2024-12-06 16:18:35 -0300

    Ajustes de configuranaçoes e imgs dos projetos

--- src/app/sections/Projects.jsx
+++ src/app/sections/Projects.jsx
@@ -1,17 +1,47 @@
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Projects = () => (
-  <section id="projetos" data-aos="fade-down-right">
-    <div className="container portifolio-box">
-      <div className="texto">
-        <h2>Título do Texto</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-          magna...
-        </p>
+  <section id="projetos" className="pt-5 mt-5" data-aos="fade-down-right">
+    <div className="container d-flex  justify-content-center pb-5">
+      <div className="row d-flex">
+        <h2> Projetos</h2>
       </div>
-      <div className="imagem">
-        <img src="/img/imagem.jpg" alt="Descrição da Imagem" />
+    </div>
+
+    <div className="container d-flex justify-content-center portifolio-box">
+      <div className="row pt-5">
+        <div className="col-6 d-flex texto">
+          <div className="txt-magmeta">
+            <h2>Título do Texto</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
+              magna...
+            </p>
+          </div>
+        </div>
+        <div className="col-6 box-projeto">
+          <img
+            src="/img/siteMagmeta.png"
+            alt="Descrição da Imagem"
+            width="50%"
+          />
+        </div>
+        <div className="col-6 box-projeto">
+          <img
+            src="/img/siteMagmeta.png"
+            alt="Descrição da Imagem"
+            width="50%"
+          />
+        </div>
+        <div className="col-6 d-flex texto">
+          <div className="txt-magmeta">
+            <h2>Título do Texto</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
+              magna...
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
