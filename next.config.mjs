3073fbf7eb4e4ad8c115cf628fdd2bbe8f2b0bbf
Vanz-silva <vanz_silva@icloud.com>
2024-12-13 10:34:53 -0300

    Fix Ajuste Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  // assetPrefix: "https://magmeta.com.br"
+};
 
 export default nextConfig;
